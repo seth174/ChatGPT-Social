681001e90672a7afeb4637d872e28503fb63db0c
seth174 <sethfagen22@gmail.com>
2023-02-01 21:27:06 -0800

    Added fe

--- backend/src/main/java/com/seth174/chatGPTSocial/controller/Health.java
+++ backend/src/main/java/com/seth174/chatGPTSocial/controller/Health.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "/v1/test")
+@RequestMapping(value = "/v1/health")
 public class Health {
 
     @GetMapping
