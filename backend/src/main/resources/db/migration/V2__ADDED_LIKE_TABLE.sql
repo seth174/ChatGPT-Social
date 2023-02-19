fab4f489ab4bc47d8a05c7f25a173ad21e7b39dc
seth174 <sethfagen22@gmail.com>
2023-02-18 17:54:41 -0800

    Added heart table in db

---
+++ backend/src/main/resources/db/migration/V2__ADDED_LIKE_TABLE.sql
@@ -0,0 +1,9 @@
+CREATE TABLE heart (
+  id SERIAL PRIMARY KEY,
+  person_id INTEGER NOT NULL REFERENCES person (id),
+  post_id INTEGER NOT NULL,
+  deleted BIT NOT NULL DEFAULT 0::bit,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  deleted_at TIMESTAMP
+);
