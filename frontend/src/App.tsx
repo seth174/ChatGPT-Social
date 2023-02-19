07187e1932dd7224331061815eea0654d5619b0d
seth174 <sethfagen22@gmail.com>
2023-02-18 18:00:56 -0800

    Added basic structure of fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Grid from '@mui/material/Grid';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Home, Profile, Rand } from './test';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid item sm={3} xs={2}>
+        <ul>
+          <li><Link to='/home'>home</Link></li>
+          <li><Link to='/profile'>profile</Link></li>
+          <li><Link to='/whatever'>home</Link></li>
+        </ul>
+      </Grid>
+      <Grid item sm={6} xs={9}>
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/whatever" element={<Rand />}></Route>
+        </Routes>
+
+      </Grid>
+      <Grid item sm={3} xs={1}>
+        <h1>Hello</h1>
+      </Grid>
+    </Grid >
   );
 }
 
---
+++ frontend/src/test.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react"
+import React from 'react'
+
+const Home: FC = () => {
+    return <h1>Home</h1>
+}
+
+const Profile: FC = () => {
+    return <h1>Profile</h1>
+}
+
+const Rand: FC = () => {
+    return <h1>Whatever</h1>
+}
+
+export { Home, Profile, Rand }
