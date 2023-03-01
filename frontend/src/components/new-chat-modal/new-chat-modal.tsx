5a2f612f6a077f46dd402fe3920f885fff3bf40a
seth174 <sethfagen22@gmail.com>
2023-02-28 21:32:57 -0800

    Added text field and close button to modal

--- frontend/src/components/new-chat-modal/new-chat-modal.tsx
+++ frontend/src/components/new-chat-modal/new-chat-modal.tsx
@@ -1,6 +1,7 @@
-import { Modal, Box, Typography, Button } from "@mui/material";
+import { Modal, Box, Typography, Button, TextField } from "@mui/material";
 import { FC, SetStateAction } from "react";
 import './new-chat-modal.css'
+import CloseIcon from '@mui/icons-material/Close';
 
 interface INewChatModalProps {
     open: boolean,
@@ -15,7 +16,12 @@ const NewChatModal: FC<INewChatModalProps> = (props: INewChatModalProps) => {
             <div className={"dark-bg"} onClick={() => props.setOpen(false)} />
             <div className={"centered"}>
                 <div className={"modal"}>
-                    <Button>Click me</Button>
+                    <div className="close-icon-border" onClick={() => props.setOpen(false)}>
+                        <CloseIcon className="close-icon" />
+                    </div>
+                    <div className="text-field-border">
+                        <TextField label={"What question can I answer?"} variant="standard" multiline fullWidth maxRows={50} />
+                    </div>
                 </div>
             </div>
         </div>
