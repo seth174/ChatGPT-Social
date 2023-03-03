92938277e0c4861e14a8027adc445f9bc3a5cd03
seth174 <sethfagen22@gmail.com>
2023-03-02 23:26:29 -0800

    Disabled cors

--- backend/src/main/java/com/seth174/chatGPTSocial/ChatGptSocialApplication.java
+++ backend/src/main/java/com/seth174/chatGPTSocial/ChatGptSocialApplication.java
@@ -1,9 +1,12 @@
 package com.seth174.chatGPTSocial;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class ChatGptSocialApplication {
 
 	public static void main(String[] args) {
