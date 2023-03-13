09291cf69e6ee835f2d8f578913b2c1b7ad49652
seth174 <sethfagen22@gmail.com>
2023-03-12 17:17:59 -0700

    changed gitignore

--- backend/dockerDatabase.sh
+++
@@ -1,9 +0,0 @@
-docker pull postgres:latest
-docker volume create postgresChatGPT
-docker run -d \
-	--name ChatGPT \
-	-e POSTGRES_PASSWORD=postgres \
-    -e PGDATA=/var/lib/postgresql/data/pgdata \
-	-v postgres:/var/lib/postgresql/data \
-    -p 5432:5432 \
-	postgres:latest
